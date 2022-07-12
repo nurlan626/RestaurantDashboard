887298ed0951d61401cfb2141c8a5eda18d3194d
nurlan007 <nurlan@artifactlabs.io>
2022-07-12 22:19:28 +0400

    fix

--- src/components/AllOrders/AllOrders.jsx
+++ src/components/AllOrders/AllOrders.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const AllOrders = () => {
-  // const menu  = JSON.parse(data)
+  const orders = useSelector((state) => state.ordersReducers);
+  let total = 0;
   return (
-    <div>asd</div>
-  )
-}
+    <div>
+      {orders.length === 0 ? (
+        <div>sifarishler yoxdur</div>
+      ) : (
+        orders.map((order, index) => {
+          return (
+            <div key={order.id}>
+              {index + 1}
+              {order.table}
+              {order.waiter} ,{order.status},{order.cost}
+              {order.status === "Sonlanib" ? (
+                <span>{order.endOrderTime}</span>
+              ) : (
+                <span>--</span>
+              )}
+              {total = total + order.cost}
+              <button>BAX</button>
+            </div>
+          );
+        })
+      )}
+      {total === 0 ? <div></div> : <div>totalAmount {total}</div>}
+      
+    </div>
+  );
+};
 
-export default AllOrders
+export default AllOrders;
